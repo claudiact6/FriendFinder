0b8844b76c6fc56050fc453e2fa00effaa73acb6
kimk <38691828+claudiact6@users.noreply.github.com>
2018-11-04 11:09:44 -0600

    fixed stylesheet serving

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
 
 var app = express();
 
-var candidates = [
+var port = 3000;
+
+var candidates = []
+
+app.use(express.static(__dirname + '/app'));
 
 
-]
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+});
 
+
+app.post("/survey", function (req, res) {
+    candidates.push(req.data);
+    console.log(candidates);
+});
+
+
+app.listen(port, function () {
+    console.log("Listening on http://localhost:" + port);
+});
