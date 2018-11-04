59fbfe34d9fcffda81b2afd5a66d6671b0ec9f0e
kimk <38691828+claudiact6@users.noreply.github.com>
2018-11-04 16:05:24 -0600

    Compatibility checker works; modal not working

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require("express");
 var path = require("path");
-var fs = require("fs");
+var bodyParser = require("body-parser");
 
 var app = express();
 
 var port = 3000;
 
-var candidates = []
+var candidates = [];
+var candidateComparison = [];
 
 app.use(express.static(__dirname + '/app'));
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
 
 
 app.get("/survey", function (req, res) {
@@ -16,12 +19,54 @@ app.get("/survey", function (req, res) {
 });
 
 
-app.post("/survey", function (req, res) {
-    candidates.push(req.data);
-    console.log(candidates);
+app.post("/api/friends", function (req, res) {
+    var parsedReqAnswers = [];
+    for (i=0; i<req.body.answers.length; i++) {
+        var answer = parseInt(req.body.answers[i]);
+        parsedReqAnswers.push(answer);
+    };
+    var parsedReqBody = {
+        name: req.body.name,
+        answers: parsedReqAnswers
+    };
+    //Empty comparison before doing new comparison
+    candidateComparison = []; 
+    //Find total difference between the new candidate and all previous candidates
+    for (i=0; i<candidates.length; i++) {
+        var differenceArray = parsedReqBody.answers.map(function(item,index) {
+            return Math.abs(item - candidates[i].answers[index]);
+        });
+        var totalDifference =  differenceArray.reduce(getSum);
+        console.log(req.body.name + "'s difference with " + candidates[i].name + ": " + totalDifference);
+        candidateComparison.push(totalDifference);
+    };
+    //Figure out which value of candidateComparison array is lowest and what candidate has that same index in the candidates array
+    console.log(candidateComparison);
+    var minimum = Array.min(candidateComparison);
+    var index = candidateComparison.indexOf(minimum);
+    console.log(index);
+    //If to avoid errors when there are 0 other candidates
+    if (candidates.length > 1) {
+    console.log(candidates[index].name);
+    var response = {
+        name: candidates[index].name,
+        pic: candidates[index].pic
+    };
+    res.send(response);
+    }
+    //Add this candidate to the list of candidates
+    candidates.push(parsedReqBody);
 });
 
 
 app.listen(port, function () {
     console.log("Listening on http://localhost:" + port);
 });
+
+function getSum(total, num) {
+    return total + num;
+};
+
+Array.min = function(array){
+    return Math.min.apply( Math, array );
+};
