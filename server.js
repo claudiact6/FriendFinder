bdf3edf973aa6833c61b0ff1e60e1aeecaa3ac9f
kimk <38691828+claudiact6@users.noreply.github.com>
2018-11-04 19:40:56 -0600

    everything works

--- server.js
+++ server.js
@@ -1,27 +1,35 @@
+//Dependencies
+
 var express = require("express");
 var path = require("path");
 var bodyParser = require("body-parser");
 
+//Setup
 var app = express();
-
 var port = 3000;
+app.use(express.static(__dirname + '/app'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+//Initialize variables
 var candidates = [];
 var candidateComparison = [];
 
-app.use(express.static(__dirname + '/app'));
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true })); 
 
 
+//HTML routing
 app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "/app/public/survey.html"));
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+});
 
+//API routing
 app.post("/api/friends", function (req, res) {
     var parsedReqAnswers = [];
-    for (i=0; i<req.body.answers.length; i++) {
+    for (i = 0; i < req.body.answers.length; i++) {
         var answer = parseInt(req.body.answers[i]);
         parsedReqAnswers.push(answer);
     };
@@ -31,45 +39,52 @@ app.post("/api/friends", function (req, res) {
         answers: parsedReqAnswers
     };
     //Empty comparison before doing new comparison
-    candidateComparison = []; 
+    candidateComparison = [];
     //Find total difference between the new candidate and all previous candidates
-    for (i=0; i<candidates.length; i++) {
-        var differenceArray = parsedReqBody.answers.map(function(item,index) {
+    for (i = 0; i < candidates.length; i++) {
+        var differenceArray = parsedReqBody.answers.map(function (item, index) {
             return Math.abs(item - candidates[i].answers[index]);
         });
-        var totalDifference =  differenceArray.reduce(getSum);
-        console.log(req.body.name + "'s difference with " + candidates[i].name + ": " + totalDifference);
+        var totalDifference = differenceArray.reduce(getSum);
         candidateComparison.push(totalDifference);
     };
-    //Figure out which value of candidateComparison array is lowest and what candidate has that same index in the candidates array
-    console.log(candidateComparison);
+    //Figure out which value of candidateComparison array is lowest and which candidate has that same index in the candidates array
     var minimum = Array.min(candidateComparison);
     var index = candidateComparison.indexOf(minimum);
-    //If to avoid errors when there are 0 other candidates
+    //If to avoid errors when there are 0 other candidates. Check compatibility with others only when there are others!
     if (candidates.length > 0) {
-    console.log(candidates[index].name);
-    var response = {
-        name: candidates[index].name,
-        pic: candidates[index].pic,
-        compatibility: (1 - (minimum / 40)) * 100
-    };
-    console.log(response);
-    res.send(response);
+        //Build response for post request, including % of compatibility (the highest possible difference between two candidates is 40)
+        var response = {
+            name: candidates[index].name,
+            pic: candidates[index].pic,
+            compatibility: ((1 - (minimum / 40)) * 100).toFixed(2),
+            firstCandidate: false
+        };
+        res.send(response);
+    } else {
+        //If this person is the first candidate, send a different response.
+        var response = {
+            pic: parsedReqBody.pic,
+            firstCandidate: true
+        }
+        res.send(response);
     };
     //Add this candidate to the list of candidates
     candidates.push(parsedReqBody);
-    console.log(candidates);
 });
 
 
-app.listen(process.env.PORT || port, function () {
-    console.log("Listening on http://localhost:" + port);
-});
-
+//Functions to do some math
 function getSum(total, num) {
     return total + num;
 };
 
-Array.min = function(array){
-    return Math.min.apply( Math, array );
+Array.min = function (array) {
+    return Math.min.apply(Math, array);
 };
+
+//Listener
+app.listen(process.env.PORT || port, function () {
+    console.log("Listening on http://localhost:" + port);
+});
+
